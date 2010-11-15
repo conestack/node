f964ec6d96fcf4d9c51900def1e5b36f2ae2468c
Florian Friesdorf <flo@chaoflow.net>
2010-11-15 19:16:26 +0100

    AbstractNode

---
+++ src/node/base.py
@@ -0,0 +1,114 @@
+from zope.interface.common.mapping import IFullMapping
+
+class AbstractNode(object):
+    """The base for all kinds of nodes, agnostic to the type of node
+
+    methods defined here are only methods that use the node itself, but make no
+    assumptions about where the nodes data is stored. Storage specific methods
+    raise NotImplementedError, that are:
+    - __delitem__
+    - __getitem__
+    - __setitem__
+    - __iter__
+    """
+    def __init__(self, name=None):
+        self.__name__ = name
+        self.__parent = None
+
+    @property
+    def path(self):
+        path = list()
+        for parent in LocationIterator(self):
+            path.append(parent.__name__)
+        path.reverse()
+        return path
+
+    @property
+    def root(self):
+        root = None
+        for parent in LocationIterator(self):
+            root = parent
+        return root
+
+    def __contains__(self, key):
+        """uses __getitem__
+        """
+        try:
+            self[key]
+        except KeyError:
+            return False
+        return True
+    
+    def __delitem__(self, key):
+        raise NotImplementedError
+
+    def __getitem__(self, key):
+        raise NotImplementedError
+
+    def __setitem__(self, key):
+        raise NotImplementedError
+
+    def __len__(self):
+        """based on keys()
+        """
+        return len(self.keys())
+
+    def __iter__(self):
+        raise NotImplementedError
+
+    def get(self, key, default=None):
+        """based on __getitem__
+        """
+        try:
+            return self[key]
+        except KeyError:
+            return default
+
+    def iterkeys(self):
+        """returns __iter__()
+        """
+        return self.__iter__()
+
+    def iteritems(self):
+        """based on __iter__ and __getitem__
+        """
+        for key in self:
+            yield key, self[key]
+
+    def itervalues(self):
+        """based on __iter and __getitem__
+        """
+        for key in self:
+            yield self[key]
+
+    def items(self):
+        """based on iteritems
+        """
+        return [x for x in self.iteritems()]
+
+    def keys(self):
+        """based on __iter__
+        """
+        return [x for x in self]
+
+    def values(self):
+        """based on itervalues
+        """
+        return [x for x in self.itervalues()]
+
+
+# XXX: WIP
+class FilteredNodespace(object):
+    def filterediter(self, flter=None):
+        """Apply filter
+
+        flter can be:
+        - interface that children need to provide
+        - dict 
+        - ...
+
+        XXX: use backend support if available, i.e. query keys from backend and
+        return only those
+
+        XXX: feels like a backend issue
+        """
