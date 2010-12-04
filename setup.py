35e584b44f38422580b5e0230b189fd93cc9afbf
Robert Niederreiter <rnix@squarewave.at>
2010-12-04 16:50:33 +0100

    add license

--- setup.py
+++ setup.py
@@ -4,6 +4,7 @@ import sys, os
 version = '0.1'
 shortdesc = "The node"
 longdesc = open(os.path.join(os.path.dirname(__file__), 'README.rst')).read()
+longdesc += open(os.path.join(os.path.dirname(__file__), 'LICENSE.rst')).read()
 
 setup(name='node',
       version=version,
