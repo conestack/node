c7904a675086e4893252e5f594302e1f3b9f8517
Robert Niederreiter <rnix@squarewave.at>
2010-12-19 23:21:16 +0100

    more work on behavior and meta class stuff

--- src/node/meta.py
+++ src/node/meta.py
@@ -1,17 +1,17 @@
+from zope.interface import implements
 from node.interfaces import (
     INode,
     IBehavior,
 )
 
 class behavior(object):
-    """Decorator to hook behaviors to Nodes.
+    """Decorator for extending nodes by behaviors.
     """
     
     def __init__(self, *behaviors):
-        for behavior in behaviors:
-            if not IBehavior.implementedBy(behavior):
-                msg = '``IBehavior`` not implemented by ``%s``'
-                msg = msg % behavior.__name__
+        for beh in behaviors:
+            if not IBehavior.implementedBy(beh):
+                msg = '``IBehavior`` not implemented by ``%s``' % beh.__name__
                 raise TypeError(msg)
         self.behaviors = behaviors
 
@@ -20,23 +20,36 @@ class behavior(object):
             msg = '``INode`` not implemented by ``%s``' % obj.__name__
             raise TypeError(msg)
         
+        # define node wrapper metaclass
         class NodeBehaviorMeta(type):
+            """Metaclass for NodeBehaviorWrapper.
+            
+            Writes behavior class objects to cls.__behaviors_cls and creates
+            empty dict cls.__behaviors_ins, which later contains the bahavior
+            class instances.
+            """
             
             def __init__(cls, name, bases, dct):
-                # XXX: validate name clashes
                 setattr(cls, '__behaviors_cls', self.behaviors)
                 setattr(cls, '__behaviors_ins', dict())
                 super(NodeBehaviorMeta, cls).__init__(name, bases, dct)
         
+        # define wrapper for decorated node
         class NodeBehaviorWrapper(obj):
+            """Derives from given ``obj`` by decorator and wrap node behavior.
+            """
             
             __metaclass__ = NodeBehaviorMeta
             __default_marker = object()
             
+            implements(INode)
+            
             def __getattribute__(self, name):
+                # try to return requested attribute from self (the node)
+                # ``super`` is at such places confusing and seem to be buggy as
+                # well. address directly where we want to do something.
                 try:
-                    return super(NodeBehaviorWrapper,
-                                 self).__getattribute__(name)
+                    return obj.__getattribute__(self, name)
                 except AttributeError, e:
                     behaviors = obj.__getattribute__(self, '__behaviors_cls')
                     ins = obj.__getattribute__(self, '__behaviors_ins')
@@ -52,11 +65,17 @@ class behavior(object):
                     raise AttributeError(name)
             
             def __repr__(self):
-                name = self.__class__.__name__
-                of_name = self.__class__.__bases__[0].__name__
-                return "<%s object of '%s' at %s>" % (name, of_name,
-                                                      hex(id(self))[:-1])
+                name = unicode(self.__name__).encode('ascii', 'replace')
+                return "<%s object '%s' at %s>" % (obj.__name__,
+                                                   name,
+                                                   hex(id(self))[:-1])
             
             __str__ = __repr__
+            
+            @property
+            def noderepr(self):
+                return "<%s object of '%s' at %s>" % (self.__class__.__name__,
+                                                      obj.__name__,
+                                                      hex(id(self))[:-1])
         
         return NodeBehaviorWrapper
