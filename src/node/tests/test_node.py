da7e4015f30fd03236a5b8ed408050c6c9daf3d5
Robert Niederreiter <rnix@squarewave.at>
2010-12-20 17:17:54 +0100

    use fullmapping tester in base tests

--- src/node/tests/test_node.py
+++ src/node/tests/test_node.py
@@ -8,6 +8,7 @@ optionflags = doctest.NORMALIZE_WHITESPACE | \
               doctest.REPORT_ONLY_FIRST_FAILURE
 
 TESTFILES = [
+    'env.txt',
     '../testing/fullmapping.txt',
     '../testing/location.txt',
     '../testing/node.txt',
