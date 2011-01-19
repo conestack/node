c513ff7d6cc13184643298b3dfc4e7518b132b36
Robert Niederreiter <rnix@squarewave.at>
2011-01-19 14:41:04 +0100

    plumbings wip

--- src/node/plumbing/__init__.py
+++ src/node/plumbing/__init__.py
@@ -1,10 +1,1 @@
-from adopt import Adopt
-from alias import Alias
-from unicode import Unicode
-from wrap import Wrap
-from validate import NodeChildValidate
-from nodespace import NodeSpace
-from attributes import (
-    Attributes,
-    NodeAttributes,
-)
+#
