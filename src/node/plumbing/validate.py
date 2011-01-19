eacc7dddc8f71e5c5d656dcb515bc2424d07d291
Robert Niederreiter <rnix@squarewave.at>
2011-01-19 14:29:44 +0100

    plumbings wip

---
+++ src/node/plumbing/validate.py
@@ -0,0 +1,24 @@
+import inspect
+from plumber import (
+    plumb,
+    default,
+)
+from zope.interface import implements
+from node.interfaces import (
+    INode,
+    INodeChildValidate,
+)
+
+class NodeChildValidate(object):
+    
+    implements(INodeChildValidate)
+    
+    allow_non_node_childs = default(False)
+    
+    @plumb
+    def __setitem__(plb, _next, self, key, val):
+        if not self.allow_non_node_childs and inspect.isclass(val):
+            raise ValueError, u"It isn't allowed to use classes as values."
+        if not self.allow_non_node_childs and not INode.providedBy(val):
+            raise ValueError("Non-node childs are not allowed.")
+        _next(self, key, val)
