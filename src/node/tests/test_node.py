b9bb59e2882041dc449e5cf5f85f92eaa774e9c9
Robert Niederreiter <rnix@squarewave.at>
2011-01-19 22:28:18 +0100

    tests for validate

--- src/node/tests/test_node.py
+++ src/node/tests/test_node.py
@@ -18,7 +18,7 @@ TESTFILES = [
     '../locking.txt',
     
     '../plumbing/adopt.txt',
-    #'../plumbing/validate.txt',
+    '../plumbing/validate.txt',
     '../plumbing/alias.txt',
     '../plumbing/attributes.txt',
     '../plumbing/nodespace.txt',
