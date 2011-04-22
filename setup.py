196eaafe2f04f1e28db0e66e06a7bedb21f23a67
Florian Friesdorf <flo@chaoflow.net>
2011-04-22 19:42:35 +0200

    0.9.3dev

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 import os
 
-version = '0.9.2'
+version = '0.9.3dev'
 shortdesc = "The node"
 longdesc = open(os.path.join(os.path.dirname(__file__), 'README.rst')).read()
 longdesc += open(os.path.join(os.path.dirname(__file__), 'LICENSE.rst')).read()
