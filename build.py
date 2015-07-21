629fdf0d91e9a442d788186f283b8517fea58345
Robert Niederreiter <office@squarewave.at>
2015-07-21 14:15:43 +0200

    build and setup works

--- build.py
+++ build.py
@@ -1,103 +1,64 @@
-"""Building the package:
-
-Look into pyc::
-
-    git clone https://github.com/gstarnberger/uncompyle.git
-    cd uncompyle/
-    sudo ./setup.py install
-    uncompyler.py pyc_file_to_decompile.pyc > recovered_file.py
-
+"""
 Building sdist::
 
     python build.py clean
-    python build.py sdist
+    python build.py -build_ext --inplace
 
-Build bdist_egg::
+Create binary distribution egg::
 
     python setup.py bdist_egg --exclude-source-files
 """
 
 
-from setuptools import find_packages
+# never sort this imports
 from setuptools import setup
 from Cython.Build import cythonize
-from distutils.extension import Extension
-import os
+from distutils.extension import Extension as Ext
 import subprocess
 import sys
 
 
 extensions = [
-    Extension('node.base', ['src/node/base.py']),
-    Extension('node.events', ['src/node/events.py']),
-    Extension('node.locking', ['src/node/locking.py']),
-    Extension('node.parts', ['src/node/parts.py']),
+    Ext('node.base', ['src/node/base.py']),
+    Ext('node.events', ['src/node/events.py']),
+    Ext('node.locking', ['src/node/locking.py']),
+    Ext('node.parts', ['src/node/parts.py']),
     # test fails right now
-    # Extension('node.utils', ['src/node/utils.py']),
-    Extension('node.behaviors.alias', ['src/node/behaviors/alias.py']),
-    Extension('node.behaviors.attributes', ['src/node/behaviors/attributes.py']),
-    Extension('node.behaviors.cache', ['src/node/behaviors/cache.py']),
-    Extension('node.behaviors.common', ['src/node/behaviors/common.py']),
-    Extension('node.behaviors.lifecycle', ['src/node/behaviors/lifecycle.py']),
-    Extension('node.behaviors.mapping', ['src/node/behaviors/mapping.py']),
-    Extension('node.behaviors.nodespace', ['src/node/behaviors/nodespace.py']),
-    Extension('node.behaviors.nodify', ['src/node/behaviors/nodify.py']),
-    Extension('node.behaviors.order', ['src/node/behaviors/order.py']),
-    Extension('node.behaviors.reference', ['src/node/behaviors/reference.py']),
-    Extension('node.behaviors.storage', ['src/node/behaviors/storage.py']),
+    # Ext('node.utils', ['src/node/utils.py']),
+    Ext('node.behaviors.alias', ['src/node/behaviors/alias.py']),
+    Ext('node.behaviors.attributes', ['src/node/behaviors/attributes.py']),
+    Ext('node.behaviors.cache', ['src/node/behaviors/cache.py']),
+    Ext('node.behaviors.common', ['src/node/behaviors/common.py']),
+    Ext('node.behaviors.lifecycle', ['src/node/behaviors/lifecycle.py']),
+    Ext('node.behaviors.mapping', ['src/node/behaviors/mapping.py']),
+    Ext('node.behaviors.nodespace', ['src/node/behaviors/nodespace.py']),
+    Ext('node.behaviors.nodify', ['src/node/behaviors/nodify.py']),
+    Ext('node.behaviors.order', ['src/node/behaviors/order.py']),
+    Ext('node.behaviors.reference', ['src/node/behaviors/reference.py']),
+    Ext('node.behaviors.storage', ['src/node/behaviors/storage.py']),
 ]
+
+
+def remove_files_by_extension(ext):
+    print 'Delete {0} files'.format(ext)
+    subprocess.Popen(
+        'find . -name "*.{0}" | xargs rm'.format(ext),
+        shell=True,
+        executable='/bin/bash'
+    )
 
 
 args = sys.argv[1:]
 if 'clean' in args:
-    print 'Delete build'
-    subprocess.Popen(
-        'rm -rf build',
-        shell=True,
-        executable='/bin/bash'
-    )
-    print 'Delete C files'
-    subprocess.Popen(
-        'find . -name "*.c" | xargs rm',
-        shell=True,
-        executable='/bin/bash'
-    )
-    print 'Delete SO files'
-    subprocess.Popen(
-        'find . -name "*.so" | xargs rm',
-        shell=True,
-        executable='/bin/bash'
-    )
+    print 'Clean build'
+    subprocess.Popen('rm -rf build', shell=True, executable='/bin/bash')
+    remove_files_by_extension('pyc')
+    remove_files_by_extension('c')
+    remove_files_by_extension('so')
+    sys.exit(0)
 
 
 setup(
     name='node',
-    #packages=find_packages('src'),
-    #package_dir={'': 'src'},
-    #namespace_packages=['node'],
-    #include_package_data=True,
-    #zip_safe=True,
-    #install_requires=[
-    #    'setuptools',
-    #    'odict>=1.5.1',
-    #    'plumber>=1.3',
-    #    'zope.lifecycleevent',
-    #    'zope.deprecation',
-    #    'zope.component',
-    #],
-    #extras_require={
-    #    'py24': [
-    #        'uuid',
-    #    ],
-    #    'test': [
-    #        'interlude',
-    #        'plone.testing',
-    #        'unittest2',
-    #        'odict[test]',
-    #        'plumber[test]',
-    #    ]
-    #},
-    ext_modules=cythonize(extensions),
-    #entry_points="""
-    #"""
-    )
+    package_dir={'': 'src'},
+    ext_modules=cythonize(extensions))
