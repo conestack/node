6af776e7b7ac048f2c2b07583656c2f1c8a0fb59
Robert Niederreiter <office@squarewave.at>
2017-03-14 08:45:40 +0100

    "setup.py indentation"

--- setup.py
+++ setup.py
@@ -11,46 +11,48 @@ longdesc += open(os.path.join(os.path.dirname(__file__), 'CHANGES.rst')).read()
 longdesc += open(os.path.join(os.path.dirname(__file__), 'LICENSE.rst')).read()
 
 
-setup(name='node',
-      version=version,
-      description=shortdesc,
-      long_description=longdesc,
-      classifiers=[
-          'License :: OSI Approved :: BSD License',
-          'Intended Audience :: Developers',
-          'Topic :: Software Development',
-          'Operating System :: OS Independent',
-          'Programming Language :: Python',
-          'Programming Language :: Python :: 2.7',
-      ],
-      keywords='node tree fullmapping',
-      author='BlueDynamics Alliance',
-      author_email='dev@bluedynamics.com',
-      url='http://github.com/bluedynamics/node',
-      license='BSD',
-      packages=find_packages('src'),
-      package_dir={'': 'src'},
-      namespace_packages=['node'],
-      include_package_data=True,
-      zip_safe=True,
-      install_requires=[
-          'setuptools',
-          'odict>=1.5.1',
-          'plumber>=1.3',
-          'zope.lifecycleevent',
-          'zope.deprecation',
-          'zope.component',
-      ],
-      extras_require={
-          'py24': [
-              'uuid',
-          ],
-          'test': [
-              'interlude',
-              'plone.testing',
-              'odict[test]',
-              'plumber[test]',
-          ]
-      },
-      entry_points="""
-      """)
+setup(
+    name='node',
+    version=version,
+    description=shortdesc,
+    long_description=longdesc,
+    classifiers=[
+        'License :: OSI Approved :: BSD License',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.7',
+    ],
+    keywords='node tree fullmapping',
+    author='BlueDynamics Alliance',
+    author_email='dev@bluedynamics.com',
+    url='http://github.com/bluedynamics/node',
+    license='BSD',
+    packages=find_packages('src'),
+    package_dir={'': 'src'},
+    namespace_packages=['node'],
+    include_package_data=True,
+    zip_safe=True,
+    install_requires=[
+        'setuptools',
+        'odict>=1.5.1',
+        'plumber>=1.3',
+        'zope.lifecycleevent',
+        'zope.deprecation',
+        'zope.component',
+    ],
+    extras_require={
+        'py24': [
+            'uuid',
+        ],
+        'test': [
+            'interlude',
+            'plone.testing',
+            'odict[test]',
+            'plumber[test]',
+        ]
+    },
+    entry_points="""
+    """
+)
