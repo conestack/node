fe001c0b82098af928e40f28f62ca91c167c960f
Robert Niederreiter <office@squarewave.at>
2018-11-07 08:38:07 +0100

    Bump version

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ def read_file(name):
         return f.read()
 
 
-version = '0.9.23'
+version = '0.9.24.dev0'
 shortdesc = "Building data structures as node trees"
 longdesc = '\n\n'.join([read_file(name) for name in [
     'README.rst',
