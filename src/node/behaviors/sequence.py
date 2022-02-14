d2423bd524224ff2d160c20fb5c7b3ffe9c75c48
Robert Niederreiter <office@squarewave.at>
2022-02-14 17:32:12 +0100

    Fix python 2

--- src/node/behaviors/sequence.py
+++ src/node/behaviors/sequence.py
@@ -1,12 +1,13 @@
 # -*- coding: utf-8 -*-
-try:
+try:  # pragma: no cover
     from collections.abc import MutableSequence as ABCMutableSequence
     from collections.abc import Sequence as ABCSequence
-except ImportError:
-    from _abccoll import MutableSequence as ABCMutableSequence
-    from _abccoll import Sequence as ABCSequence
+except ImportError:  # pragma: no cover
+    from collections import MutableSequence as ABCMutableSequence
+    from collections import Sequence as ABCSequence
 from node.behaviors import Node
 from node.behaviors.common import adopt_node
+from node.compat import IS_PY2
 from node.interfaces import ISequenceNode
 from node.interfaces import ISequenceStorage
 from node.utils import instance_property
@@ -17,15 +18,35 @@ from plumber import plumb
 from zope.interface import implementer
 from zope.interface.common.collections import IMutableSequence
 from zope.interface.common.collections import ISequence
+import functools
+import types
+
+
+def copy_func(f):
+    """Copy function.
+
+    Based on http://stackoverflow.com/a/6528148/190597 (Glenn Maynard)
+    """
+    fn = types.FunctionType(
+        f.func_code if IS_PY2 else f.__code__,
+        f.func_globals if IS_PY2 else f.__globals__,
+        name=f.func_name if IS_PY2 else f.__name__,
+        argdefs=f.func_defaults if IS_PY2 else f.__defaults__,
+        closure=f.func_closure if IS_PY2 else f.__closure__
+    )
+    fn = functools.update_wrapper(fn, f)
+    if not IS_PY2:  # pragma: no cover
+        fn.__kwdefaults__ = f.__kwdefaults__
+    return fn
 
 
 @implementer(ISequence)
 class Sequence(Behavior):
-    __contains__ = default(ABCSequence.__contains__)
-    __iter__ = default(ABCSequence.__iter__)
-    __reversed__ = default(ABCSequence.__reversed__)
-    count = default(ABCSequence.count)
-    index = default(ABCSequence.index)
+    __contains__ = default(copy_func(ABCSequence.__contains__))
+    __iter__ = default(copy_func(ABCSequence.__iter__))
+    __reversed__ = default(copy_func(ABCSequence.__reversed__))
+    count = default(copy_func(ABCSequence.count))
+    index = default(copy_func(ABCSequence.index))
 
     @default
     def __len__(self):
@@ -38,13 +59,14 @@ class Sequence(Behavior):
 
 @implementer(IMutableSequence)
 class MutableSequence(Sequence):
-    __iadd__ = default(ABCMutableSequence.__iadd__)
-    append = default(ABCMutableSequence.append)
-    clear = default(ABCMutableSequence.clear)
-    extend = default(ABCMutableSequence.extend)
-    pop = default(ABCMutableSequence.pop)
-    remove = default(ABCMutableSequence.remove)
-    reverse = default(ABCMutableSequence.reverse)
+    __iadd__ = default(copy_func(ABCMutableSequence.__iadd__))
+    append = default(copy_func(ABCMutableSequence.append))
+    # Missing in python 2
+    # clear = default(copy_func(ABCMutableSequence.clear))
+    extend = default(copy_func(ABCMutableSequence.extend))
+    pop = default(copy_func(ABCMutableSequence.pop))
+    remove = default(copy_func(ABCMutableSequence.remove))
+    reverse = default(copy_func(ABCMutableSequence.reverse))
 
     @default
     def __setitem__(self, index, value):
@@ -57,6 +79,15 @@ class MutableSequence(Sequence):
     @default
     def insert(self, index, value):
         raise NotImplementedError
+
+    @default
+    def clear(self):
+        # Missing in python 2
+        try:
+            while True:
+                self.pop()
+        except IndexError:
+            pass
 
 
 @implementer(ISequenceStorage)
@@ -126,8 +157,9 @@ class SequenceNode(Node, MutableSequence):
 
     @plumb
     def detach(next_, self, index):
-        return next_(self, int(index))
+        node = next_(self, int(index))
         self._update_indices()
+        return node
 
     @default
     def _update_indices(self):
