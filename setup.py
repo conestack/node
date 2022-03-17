7beac8a3918afb26a2c26eb8f2b0cc33e31209ea
Robert Niederreiter <office@squarewave.at>
2022-03-17 12:22:28 +0100

    Back to development: 1.1

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ def read_file(name):
         return f.read()
 
 
-version = '1.0'
+version = '1.1.dev0'
 shortdesc = "Building data structures as node trees"
 longdesc = '\n\n'.join([read_file(name) for name in [
     'README.rst',
